4d6c45c3772cb213ec736c3c6fb4979402a7dc5b
NathanBergmann <nathan.bergmann@agapys.com>
2024-07-05 17:41:38 -0300

    Alterações no Form
    *  ALterações no layout do forms

--- components/product/form/form.tsx
+++ components/product/form/form.tsx
@@ -1,34 +1,29 @@
 import styles from "./form.module.scss"
-import classNames  from "classnames"
+import classNames from "classnames"
 import RootLayout from "@/app/layout";
-
 
 const Form = () => {
     return(
-        <div className={"p-5"}>
-            <form>
-                <div className="mb-3" >
+        <div className={classNames("p-5", styles.formContainer)}>
+            <form className={styles.form}>
+                <div className={styles.divCenter}>
+                    <div>
+                    </div>
+                <div className="mb-3">
                     <label htmlFor="inputID" hidden className="form-label">ID</label>
                     <input type="number" hidden disabled className="form-control" id="inputID"/>
                 </div>
                 <div className="mb-3">
-                    <label htmlFor="iName" className="form-label">Nome</label>
-                    <input type="text" className="form-control" id="iName" aria-describedby="nameHelp"/>
+                    <input type="text" placeholder="Nome:" className="form-input w-100" id="iName" aria-describedby="nameHelp"/>
+                </div>
+                <div className="mb-2">
+                    <input type="number" placeholder="Custo" className="form-input w-100" id="iPrice"/>
                 </div>
                 <div className="mb-3">
-                    <label htmlFor="iPrice" className="form-label">Preço</label>
-                    <input type="number" className="form-control" id="iPrice"/>
+                    <input type="text" placeholder="Adicione a url da imagem." className="form-input w-100" id="iFile"/>
                 </div>
-                <div className="form-check form-switch">
-                    <input type="checkbox" className="form-check-input" id="iCheck"/>
-                    <label className="form-check-label" htmlFor="iCheck">Adicionado no Catálogo</label>
+                </div>
 
-                    
-                </div>
-                <div  className="mb-3">
-                    <label className="form-label" htmlFor="iFile">Adicione uma foto do Produto</label>
-                    <input type="file" className="form-label" id="iFile"/>
-                </div>
                 <button type="submit" className="btn btn-primary">Salvar</button>
             </form>
         </div>
