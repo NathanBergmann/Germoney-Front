7dc1e200a990ec238604e966bd9b2c473d8e8573
luizakohn <luizakohn21@gmail.com>
2024-07-05 15:49:53 -0300

    Adicionada a Landing Page
    Finalização das rotas de redirecionamento do Login e do Catálogo

--- components/landingPage/landing.tsx
+++ components/landingPage/landing.tsx
@@ -1,26 +1,28 @@
 'use client' 
 
-import AppButton from "../appButton/appButton"
 import Image from "next/image"
 import logoicon from "../../app/assets/images/logoicon.png"
 import styles from "./landing.module.scss"
 import {Container, Row, Col} from "reactstrap"
+import Link from "next/link"
+import "../../app/globals.css"
 
 const Landing = () => {
     return(
         <>
         <Container fluid className={"h-100"}>
-            <Row className={"h-100 align-items-stretch"}>
+            <Row className={"h-100 p-0 align-items-stretch"}>
                 <Col xs="4" className={"p-0"}>
                     <img src="https://static.ndmais.com.br/2023/08/img-9476-533x800.jpg" className={styles.img} alt="Compras" />
                 </Col>
                 <Col>
                 <div className={"d-flex justify-content-end align-items-end"}>
                     <Row className={"p-3 w-25"}>
-                        <AppButton  label={"Login"} onClick={() => {}}/>
+                    <Link href="/user/login">
+                            <p className={styles.loginLink}>Login</p>
+                        </Link>
                     </Row>
                 </div>
-                <div className={styles.wrapContainer}>
                     <Row className={styles.containerLogo}>
                         <Image src={logoicon} width="90" height="90" className={styles.logoImage} alt={"Logo"}/>
                     </Row>
@@ -29,16 +31,15 @@ const Landing = () => {
                     </Row>
                     <div className={"d-flex justify-content-center align-items-center"}>
                     <Row className={"row w-50"}>
-                        <AppButton label={"Ver Catálogo"} onClick={() => {}}/>
+                        <Link href="/catalog">
+                            <p className={styles.btnCatalogo}>Ver Catálogo</p>
+                        </Link>
                     </Row>
                     </div>
-                </div>
                 </Col>
             </Row>
         </Container>
         </>
-
     )
-
 }
 export default Landing
